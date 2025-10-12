a9c5f00078850a2aa0ca622b16adbcc84aaaf33e
@Inforte <87519212+Inforteuz@users.noreply.github.com>
2025-10-12 17:40:19 +0500

    Update index.php

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="generator" content="CodeIgniter">
     <meta name="description" content="CodeIgniter Alternative - A lightweight, fast, and elegant PHP framework with MVC architecture">
     <link rel="icon" href="favicon.ico" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
